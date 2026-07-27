c4d2be4deeaa35d0c906e07b43386792e2a990cc
Dmitrii <dmitriilobanov3@gmail.com>
2026-07-27 11:04:29 -0500

    refactor: consolidate Prisma module ownership

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { HealthModule } from './health/health.module';
 import { TasksModule } from './tasks/tasks.module';
-import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule, HealthModule, BoardsModule, TasksModule],
--- apps/api/src/boards/boards.module.ts
+++ apps/api/src/boards/boards.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaModule } from '../prisma/prisma.module';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
